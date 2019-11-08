72b73a98d3f61f8bf710bf0547c059a4af803ecb
Renaud <demezrenaud@skynet.be>
2019-11-08 15:51:27 +0100

    bug fix

--- RNMail/RNMail.m
+++ RNMail/RNMail.m
@@ -130,7 +130,7 @@ RCT_EXPORT_METHOD(mail:(NSDictionary *)options
                 } else if ([type isEqualToString:@"xlsx"]) {
                     mimeType = @"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
                 } else if ([type isEqualToString:@"mov"]) {
-                    mimeType = @"video/quicktime"
+                    mimeType = @"video/quicktime";
                 }
                 [mail addAttachmentData:fileData mimeType:mimeType fileName:name];
             }
